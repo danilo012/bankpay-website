f4f70f6b3248957b15c0a9421a1552ab4890ce15
danilo012 <46241643+danilo012@users.noreply.github.com>
2023-04-19 04:07:50 +0800

    Update script.js

--- script.js
+++ script.js
@@ -345,3 +345,38 @@ const navBtn = document.querySelector('.btn-mobile-nav');
 nav.addEventListener('click', function (e) {
   nav.classList.toggle('nav--open');
 });
+
+////////////
+/*form*/
+
+const handleSubmit = (event) => {
+
+  event.preventDefault();
+
+  const myForm = event.target;
+
+  const formData = new FormData(myForm);
+
+  
+
+  fetch("/", {
+
+    method: "POST",
+
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+
+    body: new URLSearchParams(formData).toString(),
+
+  })
+
+    .then(() => console.log("Form successfully submitted"))
+
+    .catch((error) => alert(error));
+
+};
+
+document
+
+  .querySelector("form")
+
+  .addEventListener("submit", handleSubmit);
